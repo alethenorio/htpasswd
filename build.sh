3e16b3199a0355fc8515c3b02c89aec488e9a50f
ByteFlinger <ByteFlinger@server.fake>
2017-05-06 20:21:23 +0200

    Attempt at setting up a release

---
+++ build.sh
@@ -0,0 +1,21 @@
+NAME="htpasswd"
+BINARY="dist/${NAME}-${GOOS}-${GOARCH}"
+VERSION="${TRAVIS_TAG}"
+COMMIT="${TRAVIS_COMMIT}"
+
+if [ -z "$VERSION" ]; then
+    VERSION="${COMMIT}"
+fi
+
+if [ "$GOOS" == "windows" ]; then
+    BINARY="${BINARY}.exe"
+fi
+
+tar cfvz "${NAME}-${VERSION}-tar.gz" .
+
+go build -o "${BINARY}" -ldflags "-X main.Version=${VERSION} -X main.buildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.commitId=${TRAVIS_COMMIT}"
+
+mv "${NAME}-${VERSION}-tar.gz" dist/
+
+
+
