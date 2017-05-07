bebdf7bc587d39cad8e7fe00677a2fbaeeaa50e9
Alexandre Thenorio <alexandre.thenorio@ericsson.com>
2017-05-07 14:36:26 +0200

    Added linux build script

---
+++ buildall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+NAME="htpasswd"
+GOOS="linux"
+GOARCH="amd64"
+
+BINARY="dist/${NAME}-${GOOS}-${GOARCH}"
+
+VERSION="$(git describe --tags)"
+
+COMMITID="$(git rev-parse HEAD)"
+
+BUILDTIME="$(date -u '+%Y-%m-%d_%I:%M:%S%p')"
+
+echo "Building version=${VERSION} from commit=${COMMITID} for ${GOOS}/${GOARCH}"
+CGO_ENABLED=0 go build -a -installsuffix cgo -o "${BINARY}" -ldflags "-X main.version=${VERSION} -X main.buildTime=${BUILDTIME} -X main.commitId=${COMMITI}"
+
+GOOS=windows
+GOARCH=amd64
+BINARY="dist/${NAME}-${GOOS}-${GOARCH}.exe"
+
+echo "Building version=${VERSION} from commit=${COMMITID} for ${GOOS}/${GOARCH}"
+go build -o "${BINARY}" -ldflags "-X main.version=${VERSION} -X main.buildTime=${BUILDTIME} -X main.commitId=${COMMITID}"
